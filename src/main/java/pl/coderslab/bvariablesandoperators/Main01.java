ee2a5be6f28ac474fcaa8675ee769f16d0a09bdf
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-17 15:38:27 +0200

    created variables

--- src/main/java/pl/coderslab/bvariablesandoperators/Main01.java
+++ src/main/java/pl/coderslab/bvariablesandoperators/Main01.java
@@ -3,7 +3,11 @@ package pl.coderslab.bvariablesandoperators;
 public class Main01 {
 
 	public static void main(String[] args) {
-
+		int int1;
+		char char2;
+		long long3;
+		double double4;
+		boolean boolean5;
 	}
 
 }
