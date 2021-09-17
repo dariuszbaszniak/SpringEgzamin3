8970c42ffce4d25203220950e023ee0b8b0f6d54
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-17 14:59:07 +0200

    modified text

--- src/main/java/pl/coderslab/afirstprogram/Main03.java
+++ src/main/java/pl/coderslab/afirstprogram/Main03.java
@@ -3,8 +3,8 @@ package pl.coderslab.afirstprogram;
 public class Main03 {
 
 	public static void main(String[] args) {
-		System.out.print("Dzisiaj zaczynam kurs");
-		System.out.print("JAVA");
+		System.out.println("Dzisiaj zaczynam kurs");
+		System.out.println("JAVA");
 		System.out.print("w CodersLab");
 
 	}
