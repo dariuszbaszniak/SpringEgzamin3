2743b8382af7d5fbe41a1f27ffc11ca81f992a54
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-17 21:09:07 +0200

    cflowcontrol Main03

--- src/main/java/pl/coderslab/cflowcontrol/Main03.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main03.java
@@ -3,7 +3,17 @@ package pl.coderslab.cflowcontrol;
 public class Main03 {
 
 	public static void main(String[] args) {
+		String text = "Java";
 
+		for(int i=0; i<3; i++){
+			System.out.println(text);
+		}
+
+		int j = 0;
+		while (j < 3){
+			System.out.println(text);
+			j++;
+		}
 	}
 
 }
