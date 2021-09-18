50475c49ce963097bb724256a4e66a75b48520ea
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 11:42:09 +0200

    cflowcontrol Main07

--- src/main/java/pl/coderslab/cflowcontrol/Main07.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main07.java
@@ -4,6 +4,22 @@ public class Main07 {
 
 	public static void main(String[] args) {
 
+		for(int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				System.out.println("i = " + i + " j = " + j);
+			}
+		}
+
+		int i = 0;
+		while (i < 3){
+			int j = 0;
+			while (j < 3){
+				System.out.println("i = " + i+ " j = " + j);
+				j++;
+			}
+			i++;
+		}
+
 	}
 
 }
