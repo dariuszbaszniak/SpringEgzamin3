dbeeb8c65140c28bc919b955ee71a7869469aeb0
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 15:55:44 +0200

    estartupparameters Main03

--- src/main/java/pl/coderslab/estartupparameters/Main03.java
+++ src/main/java/pl/coderslab/estartupparameters/Main03.java
@@ -1,7 +1,16 @@
 package pl.coderslab.estartupparameters;
 
+import java.util.Locale;
+
 public class Main03 {
     public static void main(String[] args) {
 
+        double avg = 0;
+
+        for(int i =0; i < args.length; i++){
+            avg = avg + Integer.parseInt(args[i]);
+        }
+
+        System.out.println(avg/args.length);
     }
 }
