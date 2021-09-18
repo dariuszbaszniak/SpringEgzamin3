0e3175dd1140ad40bd74d3fd2c96cb5aaeaca750
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 22:35:52 +0200

    estartupparameters Main05

--- src/main/java/pl/coderslab/estartupparameters/Main05.java
+++ src/main/java/pl/coderslab/estartupparameters/Main05.java
@@ -1,9 +1,31 @@
 package pl.coderslab.estartupparameters;
 
+import org.checkerframework.checker.units.qual.A;
+import org.checkerframework.checker.units.qual.C;
+
 public class Main05 {
 
     public static void main(String[] args) {
 
+        int points = Integer.parseInt(args[0]);
+
+        if (points >= 0 && points <= 100){
+            if (points >=0 && points < 51 ){
+                System.out.println("F");
+            }
+            else if (points >=51 && points < 71){
+                System.out.println("E");
+            }
+            else if (points >= 71 && points < 91){
+                System.out.println("C");
+            }
+            else {
+                System.out.println("A");
+            }
+        }
+        else {
+            System.out.println("BADVALUE");
+        }
     }
 
 }
