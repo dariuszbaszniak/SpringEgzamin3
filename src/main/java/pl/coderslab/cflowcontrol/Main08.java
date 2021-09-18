ada23c0fc7176941996d5887631308292c91fc60
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 11:52:23 +0200

    cflowcontrol Main08

--- src/main/java/pl/coderslab/cflowcontrol/Main08.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main08.java
@@ -4,5 +4,18 @@ public class Main08 {
 
     public static void main(String[] args) {
         int n = 5;
+
+        String star = "*";
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                if(i >= j) {
+                    System.out.print(star);
+                } else {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
     }
 }
