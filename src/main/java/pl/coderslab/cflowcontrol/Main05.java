b2ca008cbca6e759a396b2f6303e2e44646457ee
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 11:07:54 +0200

    cflowcontrol Main05

--- src/main/java/pl/coderslab/cflowcontrol/Main05.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main05.java
@@ -3,7 +3,20 @@ package pl.coderslab.cflowcontrol;
 public class Main05 {
 
 	public static void main(String[] args) {
+		int resultFor = 0;
+		int resultWhile = 0;
 
+		for(int i=1; i<11; i++){
+			resultFor = resultFor + i;
+		}
+
+		int j = 1;
+		while (j < 11){
+			resultWhile = resultWhile + j;
+			j++;
+		}
+		System.out.println(resultFor);
+		System.out.println(resultWhile);
 	}
 
 }
