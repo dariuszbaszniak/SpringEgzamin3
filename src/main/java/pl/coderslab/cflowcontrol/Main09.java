d6125513ff38e8776528b01cb883e419fbefe14b
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 11:58:25 +0200

    cflowcontrol Main09

--- src/main/java/pl/coderslab/cflowcontrol/Main09.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main09.java
@@ -3,6 +3,17 @@ package pl.coderslab.cflowcontrol;
 public class Main09 {
 
     public static void main(String[] args) {
+
         int n = 5;
+        String star = "*";
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if (i >= j){
+                    System.out.print(star);
+                }
+            }
+            System.out.println();
+        }
     }
 }
