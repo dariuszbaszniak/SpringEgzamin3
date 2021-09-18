76f7f923ac83c89b3a539b6e9c14da678e2b1509
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 11:27:15 +0200

    cflowcontrol Main06

--- src/main/java/pl/coderslab/cflowcontrol/Main06.java
+++ src/main/java/pl/coderslab/cflowcontrol/Main06.java
@@ -3,7 +3,31 @@ package pl.coderslab.cflowcontrol;
 public class Main06 {
 
 	public static void main(String[] args) {
+
 		int n = 6;
+
+		for (int i=0; i<=n; i++){
+			int result = i%2;
+			if (result==0){
+				System.out.println(i+ " - parzysta");
+			}
+			else{
+				System.out.println(i+ " - nieparzysta");
+			}
+		}
+
+		int j=0;
+		while(j<=n){
+			int result = j%2;
+			if (result==0){
+				System.out.println(j+ " - parzysta");
+			}
+			else{
+				System.out.println(j+ " - nieparzysta");
+			}
+			j++;
+		}
+
 	}
 
 }
