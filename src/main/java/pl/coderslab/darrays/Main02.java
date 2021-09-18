1c0be3eb7d9fe3bd0f75b3355451d13569caf00f
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-18 14:09:34 +0200

    darrays Main02

--- src/main/java/pl/coderslab/darrays/Main02.java
+++ src/main/java/pl/coderslab/darrays/Main02.java
@@ -3,7 +3,11 @@ package pl.coderslab.darrays;
 public class Main02 {
 
 	public static void main(String[] args) {
+		String[] fruits = new String[] {"apple","berry","apple","banana","berry"};
 
+		for(int i = 0; i<fruits.length; i++){
+			System.out.println(fruits[i]);
+		}
 	}
 
 }
