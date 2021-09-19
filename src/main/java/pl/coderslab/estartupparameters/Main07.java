7db6dbac34e26ff61cc17bf04e634b22a4ff7886
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-19 09:51:31 +0200

    estartupparameters Main07

--- src/main/java/pl/coderslab/estartupparameters/Main07.java
+++ src/main/java/pl/coderslab/estartupparameters/Main07.java
@@ -3,7 +3,13 @@ package pl.coderslab.estartupparameters;
 public class Main07 {
 
     public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        int factorial=1;
 
+        for(int i = 1; i <= number; i++){
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
     }
 
 }
