ea59b9c98e9fc7de9e144c6121ec4a737fa3ba92
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-19 09:42:08 +0200

    estartupparameters Main06

--- src/main/java/pl/coderslab/estartupparameters/Main06.java
+++ src/main/java/pl/coderslab/estartupparameters/Main06.java
@@ -3,7 +3,13 @@ package pl.coderslab.estartupparameters;
 public class Main06 {
 
     public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
 
+        for(int i = 1; i <= number; i++){
+            for(int j = 1; j <= number; j++){
+                System.out.println(i+ " x " + j +" = "+ i*j);
+            }
+        }
     }
 
 }
