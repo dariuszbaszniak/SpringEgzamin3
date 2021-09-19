de15347d4e613e1cab8cc1336347b40930cb8005
Dariusz Baszniak <dariuszbaszniak@wp.pl>
2021-09-19 12:08:13 +0200

    estartupparameters Main08

--- src/main/java/pl/coderslab/estartupparameters/Main08.java
+++ src/main/java/pl/coderslab/estartupparameters/Main08.java
@@ -3,7 +3,16 @@ package pl.coderslab.estartupparameters;
 public class Main08 {
 
     public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
 
+        int variable = 0;
+
+            while (x % y != 0) {
+                variable = y;
+                y = x % y;
+                x = variable;
+            }
+            System.out.println(y);
     }
-
 }
