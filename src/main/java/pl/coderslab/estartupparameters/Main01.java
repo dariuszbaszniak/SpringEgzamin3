90c30dbc91eee36c030c910fbfe29754d94ca2e2
Darek <dariuszbaszniak@wp.pl>
2022-01-17 17:23:22 +0100

    create project

--- src/main/java/pl/coderslab/estartupparameters/Main01.java
+++
@@ -1,9 +0,0 @@
-package pl.coderslab.estartupparameters;
-
-public class Main01 {
-    public static void main(String[] args) {
-        for(int i = 0 ; i < args.length; i++){
-            System.out.print(args[i]+" ");
-        }
-    }
-}
